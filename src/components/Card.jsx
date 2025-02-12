59bdb5d5bbc2a0808f02e39f34e0d322559a05b2
Umberto Amenta <umberto.amenta@hotmail.it>
2025-02-12 23:38:53 +0100

    feat: +country-flag

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,56 @@
 export default function Card(media) {
+  const flag = {
+    it: "IT",
+    fr: "FR",
+    es: "ES",
+    us: "US",
+    en: "GB",
+    de: "DE",
+    pt: "PT",
+    nl: "NL",
+    be: "BE",
+    se: "SE",
+    ch: "CH",
+    at: "AT",
+    no: "NO",
+    dk: "DK",
+    fi: "FI",
+    ie: "IE",
+    ar: "SA",
+    ko: "KR",
+    cn: "CN",
+    ja: "JP",
+    hi: "IN",
+    ru: "RU",
+    xx: "XX",
+    tr: "TR",
+    tl: "PH",
+  };
+
+  const flagImg = flag[media.original_language]
+    ? `https://purecatamphetamine.github.io/country-flag-icons/3x2/${
+        flag[media.original_language]
+      }.svg`
+    : media.original_language;
+
   return (
     <>
       <li key={media.id}>
         <ul>
-          <li>{media.title || media.name}</li>
-          <li>{media.original_title || media.original_name}</li>
-          <li>{media.original_language}</li>
-          <li>{media.overview}</li>
+          {/* <li>{media.title || media.name}</li>
+          <li>{media.original_title || media.original_name}</li> */}
+          <li>
+            {flagImg ? (
+              <img
+                className="flag"
+                src={flagImg}
+                alt={media.original_language}
+              />
+            ) : (
+              media.original_language
+            )}{" "}
+          </li>
+          {/* <li>{media.overview}</li> */}
         </ul>
       </li>
     </>
